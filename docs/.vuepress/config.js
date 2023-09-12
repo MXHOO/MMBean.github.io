846919976a4db4e902989b722806319a6101e228
mxh <mxh@mxhdeMacBook-Pro.local>
2023-09-12 11:18:55 +0800

    fix: 注释配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
-import { defineClientConfig, defineUserConfig } from "@vuepress/client";
-export default defineUserConfig({
-  lang: 'zh-CN',
-  title: 'hello',
-  description: 'desc'
-})
+// import { defineClientConfig, defineUserConfig } from "@vuepress/client";
+// export default defineUserConfig({
+//   lang: 'zh-CN',
+//   title: 'hello',
+//   description: 'desc'
+// })
